feab586626ae0190c8398e29d16c62faf05ee069
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-09-03 17:47:12 +0900

    update product job

--- packages/shopify/microCMS.ts
+++ packages/shopify/microCMS.ts
@@ -1,25 +1,28 @@
-import { createClient } from 'microcms-js-sdk'
+import { createClient } from "microcms-js-sdk";
 
 const cmsClient = createClient({
   serviceDomain: "survaq-shopify",
-  apiKey: process.env.MICROCMS_API_TOKEN ?? '',
+  apiKey: process.env.MICROCMS_API_TOKEN ?? "",
 });
 
 type ProductOnMicroCMS = {
   productIds: string;
-  productName: string
+  productName: string;
 };
 
-
-export const getProductOnMicroCMS = async (id: string | number) => {
+export const getProductOnMicroCMS = async (id: string) => {
   const {
     contents: [product],
   } = await cmsClient.getList<ProductOnMicroCMS>({
     endpoint: "products",
     queries: {
-      filters: "productIds[contains]" + id,
+      filters:
+        "productIds[contains]" + id.replace("gid://shopify/Product/", ""),
     },
   });
 
-  return { productGroupId: product?.id ?? null, productGroupName: product?.productName ?? null }
-}
+  return {
+    productGroupId: product?.id ?? null,
+    productGroupName: product?.productName ?? null,
+  };
+};
