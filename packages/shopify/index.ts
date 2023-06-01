2992dedd704637114014a0f1b72d276c4d1554e6
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-06-01 23:00:00 +0900

    bugfix shopify job

--- packages/shopify/index.ts
+++ packages/shopify/index.ts
@@ -117,6 +117,7 @@ export const products = async (): Promise<void> => {
     hasNext = data.products.pageInfo.hasNextPage;
 
     data.products.edges.forEach((edge) => {
+      cursor = edge.cursor;
       const group = productIdAndGroupMappings.find(({ productIds }) =>
         productIds.includes(edge.node.id)
       );
